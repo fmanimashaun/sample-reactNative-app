b6372368b0dc72b3850c81de48185ea7cc7c5f4e
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-08-02 00:38:54 +0100

    Update setting

--- App.tsx
+++ App.tsx
@@ -27,9 +27,9 @@ const styles = StyleSheet.create({
     backgroundColor: "ghostwhite",
   },
   box: {
-    width: 300,
     height: 100,
     justifyContent: "center",
+    alignSelf: "stretch",
     alignItems: "center",
     backgroundColor: "lightgray",
     borderWidth: 1,
